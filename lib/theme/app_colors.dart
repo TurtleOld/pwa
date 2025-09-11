92f549abcbf33f8b80c8a8306251e7c7d8522e4c
Alexander Pavlov <dev@pavlovteam.ru>
2025-09-11 10:25:07 +0300

    add auth page + plug

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Primary colors (from Django CSS variables)
+  static const Color primary = Color(0xFF3273DC);
+  static const Color primaryLight = Color(0xFF4FC08D);
+  static const Color primaryDark = Color(0xFF205081);
+  static const Color secondary = Color(0xFF4FC08D);
+  static const Color secondaryLight = Color(0xFF48C774);
+  static const Color secondaryDark = Color(0xFF3EC46D);
+
+  // Status colors
+  static const Color success = Color(0xFF48C774);
+  static const Color warning = Color(0xFFFFDD57);
+  static const Color danger = Color(0xFFFF3860);
+  static const Color info = Color(0xFF209CEE);
+
+  // Neutral colors
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color gray50 = Color(0xFFF8F9FA);
+  static const Color gray100 = Color(0xFFF5F5F5);
+  static const Color gray200 = Color(0xFFE9ECEF);
+  static const Color gray300 = Color(0xFFDEE2E6);
+  static const Color gray400 = Color(0xFFCED4DA);
+  static const Color gray500 = Color(0xFFADB5BD);
+  static const Color gray600 = Color(0xFF6C757D);
+  static const Color gray700 = Color(0xFF495057);
+  static const Color gray800 = Color(0xFF343A40);
+  static const Color gray900 = Color(0xFF212529);
+
+  // Text colors
+  static const Color textPrimary = Color(0xFF2C3E50);
+  static const Color textSecondary = Color(0xFF6C757D);
+  static const Color textMuted = Color(0xFFB5B5B5);
+  static const Color textLight = Color(0xFFF5F5F5);
+
+  // Background colors
+  static const Color bgPrimary = Color(0xFFFFFFFF);
+  static const Color bgSecondary = Color(0xFFF8F9FA);
+  static const Color bgTertiary = Color(0xFFE9ECEF);
+  static const Color bgDark = Color(0xFF121212);
+  static const Color bgDarkSecondary = Color(0xFF2C313A);
+  static const Color bgDarkTertiary = Color(0xFF23272F);
+
+  // Border colors
+  static const Color borderLight = Color(0xFFDBDBDB);
+  static const Color borderMedium = Color(0xFFB5B5B5);
+  static const Color borderDark = Color(0xFF4A4A4A);
+
+  // Dark theme colors
+  static const Color darkTextPrimary = Color(0xFFF5F5F5);
+  static const Color darkTextSecondary = Color(0xFFB5B5B5);
+  static const Color darkTextMuted = Color(0xFF7A7A7A);
+  static const Color darkBgPrimary = Color(0xFF121212);
+  static const Color darkBgSecondary = Color(0xFF2C2C2C);
+  static const Color darkBgTertiary = Color(0xFF363636);
+  static const Color darkBorderLight = Color(0xFF404040);
+  static const Color darkBorderMedium = Color(0xFF505050);
+  static const Color darkBorderDark = Color(0xFF606060);
+}
